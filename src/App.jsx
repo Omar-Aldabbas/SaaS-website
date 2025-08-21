5fcd8c614e6eacfb2d6e3704cb89dfb8e5af0a87
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-08-21 07:10:12 -0700

    Fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Navbar } from "./components/Navbar";
-import './index.css';
 import { BackgroundEffect } from "./components/BackgroundEffect";
 import { Footer } from "./components/Footer";
 import { Blog } from "./pages/Blog";
@@ -11,22 +9,21 @@ import { About } from "./pages/About";
 import { FAQ } from "./pages/FAQ";
 import { Signup } from "./pages/Signup";
 
-
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <BackgroundEffect />
-      <Navbar /> 
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/faq" element={<FAQ/>}/>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
